55dfc2496e32ffd11d6f6c29fb3a747ce469d591
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-08-17 20:04:11 +0200

    setup cors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import 'dotenv/config';
 import openaiRoutes from './routes/openai';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/openai', openaiRoutes);
 
